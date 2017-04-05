639cdabf62d1cc7f0f8668f24db6a29d584cb943
Elena <lena_dreames@mail.ru>
2017-04-05 12:56:31 +0300

    version 1.1

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/*
+ * Third party
+ */
+
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/*
+ * Custom
+ */
+//= partials/bootstrap.min.js
+//= partials/owl.carousel.min.js
+//= partials/jquery.carousel.js
+//= partials/app.js
